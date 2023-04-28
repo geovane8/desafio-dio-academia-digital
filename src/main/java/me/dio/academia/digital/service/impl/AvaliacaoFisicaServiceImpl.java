2f0d1c95a66e76a643d4842b563a85e069e335b7
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-04-28 16:07:16 -0300

    Update AvaliacaoFisicaServiceImpl.java

--- src/main/java/me/dio/academia/digital/service/impl/AvaliacaoFisicaServiceImpl.java
+++ src/main/java/me/dio/academia/digital/service/impl/AvaliacaoFisicaServiceImpl.java
@@ -4,6 +4,7 @@ import me.dio.academia.digital.entity.Aluno;
 import me.dio.academia.digital.entity.AvaliacaoFisica;
 import me.dio.academia.digital.entity.form.AvaliacaoFisicaForm;
 import me.dio.academia.digital.entity.form.AvaliacaoFisicaUpdateForm;
+import me.dio.academia.digital.exception.ResourceNotFoundException;
 import me.dio.academia.digital.repository.AlunoRepository;
 import me.dio.academia.digital.repository.AvaliacaoFisicaRepository;
 import me.dio.academia.digital.service.IAvaliacaoFisicaService;
@@ -15,42 +16,59 @@ import java.util.List;
 @Service
 public class AvaliacaoFisicaServiceImpl implements IAvaliacaoFisicaService {
 
-  @Autowired
-  private AvaliacaoFisicaRepository avaliacaoFisicaRepository;
+    private final AvaliacaoFisicaRepository avaliacaoFisicaRepository;
+    private final AlunoRepository alunoRepository;
 
-  @Autowired
-  private AlunoRepository alunoRepository;
+    @Autowired
+    public AvaliacaoFisicaServiceImpl(AvaliacaoFisicaRepository avaliacaoFisicaRepository, AlunoRepository alunoRepository) {
+        this.avaliacaoFisicaRepository = avaliacaoFisicaRepository;
+        this.alunoRepository = alunoRepository;
+    }
 
-  @Override
-  public AvaliacaoFisica create(AvaliacaoFisicaForm form) {
-    AvaliacaoFisica avaliacaoFisica = new AvaliacaoFisica();
-    Aluno aluno = alunoRepository.findById(form.getAlunoId()).get();
+    @Override
+    public AvaliacaoFisica create(AvaliacaoFisicaForm form) {
+        Aluno aluno = alunoRepository.findById(form.getAlunoId())
+                .orElseThrow(() -> new ResourceNotFoundException("Aluno não encontrado com id " + form.getAlunoId()));
 
-    avaliacaoFisica.setAluno(aluno);
-    avaliacaoFisica.setAluno(form.getPeso());
-    avaliacaoFisica.setAltura(form.getAltura());
+        AvaliacaoFisica avaliacaoFisica = new AvaliacaoFisica();
+        avaliacaoFisica.setAluno(aluno);
+        avaliacaoFisica.setPeso(form.getPeso());
+        avaliacaoFisica.setAltura(form.getAltura());
 
-    return avaliacaoFisicaRepository.save(avaliacaoFisica);
-  }
+        return avaliacaoFisicaRepository.save(avaliacaoFisica);
+    }
 
-  @Override
-  public AvaliacaoFisica get(Long id) {
-    return null;
-  }
+    @Override
+    public AvaliacaoFisica get(Long id) {
+        return avaliacaoFisicaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Avaliação física não encontrada com id " + id));
+    }
 
-  @Override
-  public List<AvaliacaoFisica> getAll() {
+    @Override
+    public List<AvaliacaoFisica> getAll() {
+        return avaliacaoFisicaRepository.findAll();
+    }
 
-    return avaliacaoFisicaRepository.findAll();
-  }
+    @Override
+    public AvaliacaoFisica update(Long id, AvaliacaoFisicaUpdateForm formUpdate) {
+        AvaliacaoFisica avaliacaoFisica = avaliacaoFisicaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Avaliação física não encontrada com id " + id));
 
-  @Override
-  public AvaliacaoFisica update(Long id, AvaliacaoFisicaUpdateForm formUpdate) {
-    return null;
-  }
+        if (formUpdate.getPeso() != null) {
+            avaliacaoFisica.setPeso(formUpdate.getPeso());
+        }
+        if (formUpdate.getAltura() != null) {
+            avaliacaoFisica.setAltura(formUpdate.getAltura());
+        }
 
-  @Override
-  public void delete(Long id) {
+        return avaliacaoFisicaRepository.save(avaliacaoFisica);
+    }
 
-  }
+    @Override
+    public void delete(Long id) {
+        AvaliacaoFisica avaliacaoFisica = avaliacaoFisicaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Avaliação física não encontrada com id " + id));
+
+        avaliacaoFisicaRepository.delete(avaliacaoFisica);
+    }
 }

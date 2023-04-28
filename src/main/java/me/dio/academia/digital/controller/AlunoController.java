3649f2811db57b8e74efcd9a485774d4c5b047fd
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-04-28 15:23:28 -0300

    Update AlunoController.java
    Principais melhorias aplicadas:
    
    Uso da anotação @Validated para habilitar a validação dos parâmetros de entrada;
    Utilização de injeção de dependência através do construtor ao invés da anotação @Autowired;
    Utilização da anotação @ResponseStatus(HttpStatus.CREATED) para indicar que a operação de criação retornará o código HTTP 201 (Created);
    Utilização da classe ResponseEntity para encapsular a resposta da requisição com código HTTP e possíveis headers;
    Melhorias no tratamento de erros, retornando códigos HTTP adequados e uma mensagem indicativa do erro.

--- src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -5,41 +5,56 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.dio.academia.digital.entity.Aluno;
 import me.dio.academia.digital.entity.AvaliacaoFisica;
 import me.dio.academia.digital.entity.form.AlunoForm;
-import me.dio.academia.digital.service.impl.AlunoServiceImpl;
+import me.dio.academia.digital.service.AlunoService;
 
 @RestController
 @RequestMapping("/alunos")
+@Validated
 public class AlunoController {
 
+  private final AlunoService alunoService;
+
   @Autowired
-  public AlunoServiceImpl service;
+  public AlunoController(AlunoService alunoService) {
+      this.alunoService = alunoService;
+  }
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public Aluno create(@Valid @RequestBody AlunoForm form) {
-    return service.create(form);
+    return alunoService.create(form);
   }
 
   @GetMapping("/avaliacoes/{id}")
-  public List<AvaliacaoFisica> getAllAvaliacaoFisicaId(@PathVariable Long id) {
-    return service.getAllAvaliacaoFisicaId(id);
+  public ResponseEntity<List<AvaliacaoFisica>> getAllAvaliacaoFisicaId(@PathVariable Long id) {
+    List<AvaliacaoFisica> avaliacoes = alunoService.getAllAvaliacaoFisicaId(id);
+    if(avaliacoes.isEmpty()) {
+        return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(avaliacoes);
   }
 
   @GetMapping
-  public List<Aluno> getAll(@RequestParam(value = "dataDeNascimento", required = false)
-                                  String dataDeNascimento){
-    return service.getAll(dataDeNascimento);
+  public ResponseEntity<List<Aluno>> getAll(@RequestParam(value = "dataDeNascimento", required = false) String dataDeNascimento){
+    List<Aluno> alunos = alunoService.getAll(dataDeNascimento);
+    if(alunos.isEmpty()) {
+        return ResponseEntity.noContent().build();
+    }
+    return ResponseEntity.ok(alunos);
   }
-
-
 }

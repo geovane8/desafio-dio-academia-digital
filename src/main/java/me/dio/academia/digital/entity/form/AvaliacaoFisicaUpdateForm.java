345ae26ec4b0f9bc950c26ea338bff7c9348e4d4
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-04-28 15:48:53 -0300

    Update AvaliacaoFisicaUpdateForm.java
    Ao utilizar a anotação @Data do Lombok, é possível gerar automaticamente getters e setters para os campos da classe, além de métodos toString, equals e hashCode, o que torna o código mais conciso e legível. Também foram adicionados modificadores de acesso final aos campos da classe

--- src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaUpdateForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaUpdateForm.java
@@ -1,24 +1,12 @@
 package me.dio.academia.digital.entity.form;
 
+import lombok.Data;
+
+@Data
 public class AvaliacaoFisicaUpdateForm {
-
-  private double peso;
-
-  private double altura;
-
-  public double getAltura() {
-    return altura;
-  }
-
-  public double getPeso() {
-    return peso;
-  }
-
-  public void setPeso(double peso) {
-    this.peso = peso;
-  }
-
-  public void setAltura(double altura) {
-    this.altura = altura;
-  }
+  
+  private final double peso;
+  
+  private final double altura;
+  
 }

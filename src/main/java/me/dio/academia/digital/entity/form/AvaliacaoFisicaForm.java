0af59ce29a3d86240501bfb7bd46fc40d0c94576
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-04-28 15:45:13 -0300

    Update AvaliacaoFisicaForm.java
    Importação das anotações de validação do pacote javax.validation.
    Alteração do tipo primitivo double para o objeto Double nas propriedades peso e altura, para permitir validações de valor nulo.
    Adição de mensagens de erro customizadas nas anotações de validação.
    Remoção dos métodos desnecessários e implementação dos métodos getters e setters com retorno e parâmetros corretos.

--- src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
@@ -1,5 +1,10 @@
 package me.dio.academia.digital.entity.form;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.time.LocalDate;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,35 +18,35 @@ public class AvaliacaoFisicaForm {
   private Long alunoId;
 
   @NotNull(message = "Preencha o campo corretamente.")
-  @Positive(message = "${validatedValue}' precisa ser positivo.")
-  private double peso;
+  @Positive(message = "{validation.positive}")
+  private Double peso;
 
   @NotNull(message = "Preencha o campo corretamente.")
-  @Positive(message = "${validatedValue}' precisa ser positivo.")
-  @DecimalMin(value = "150", message = "'${validatedValue}' precisa ser no mínimo {value}.")
-  private double altura;
+  @Positive(message = "{validation.positive}")
+  @DecimalMin(value = "150", message = "{validation.decimalMin}")
+  private Double altura;
 
-public Long getAlunoId() {
-    return null;
-}
-
-public void setAltura(double altura) {
-    this.altura = altura;
-}
-
-public void setPeso(double peso) {
-    this.peso = peso;
-}
-
-public void setAlunoId(Long alunoId) {
+  public void setAlunoId(Long alunoId) {
     this.alunoId = alunoId;
-}
+  }
 
-public Object getPeso() {
-    return null;
-}
+  public void setPeso(Double peso) {
+    this.peso = peso;
+  }
 
-public Object getAltura() {
-    return null;
-}
+  public void setAltura(Double altura) {
+    this.altura = altura;
+  }
+
+  public Long getAlunoId() {
+    return alunoId;
+  }
+
+  public Double getPeso() {
+    return peso;
+  }
+
+  public Double getAltura() {
+    return altura;
+  }
 }

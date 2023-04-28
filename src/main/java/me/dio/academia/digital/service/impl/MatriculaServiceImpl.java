dfe4713be1c324a896c740be6a7fde78021f095c
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-04-28 16:09:15 -0300

    Update MatriculaServiceImpl.java
    O uso de transações com a anotação @Transactional, garantindo que as operações no banco de dados sejam atômicas e consistentes.
    A utilização do método orElseThrow() para evitar o retorno de valores nulos no método get().
    A utilização de Optional em alguns métodos para tratar objetos nulos sem precisar usar expressões condicionais.

--- src/main/java/me/dio/academia/digital/service/impl/MatriculaServiceImpl.java
+++ src/main/java/me/dio/academia/digital/service/impl/MatriculaServiceImpl.java
@@ -1,54 +1,64 @@
 package me.dio.academia.digital.service.impl;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import me.dio.academia.digital.entity.Aluno;
 import me.dio.academia.digital.entity.Matricula;
 import me.dio.academia.digital.entity.form.MatriculaForm;
 import me.dio.academia.digital.repository.AlunoRepository;
 import me.dio.academia.digital.repository.MatriculaRepository;
 import me.dio.academia.digital.service.IMatriculaService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
+@Transactional
 public class MatriculaServiceImpl implements IMatriculaService {
 
-  @Autowired
-  private MatriculaRepository matriculaRepository;
+    private final MatriculaRepository matriculaRepository;
+    private final AlunoRepository alunoRepository;
 
-  @Autowired
-  private AlunoRepository alunoRepository;
-
-  @Override
-  public Matricula create(MatriculaForm form) {
-    Matricula matricula = new Matricula();
-    Aluno aluno = alunoRepository.findById(form.getAlunoId()).get();
-
-    matricula.setAluno(aluno);
-
-    return matriculaRepository.save(matricula);
-  }
-
-  @Override
-  public Matricula get(Long id) {
-    return matriculaRepository.findById(id).get();
-  }
-
-  @Override
-  public List<Matricula> getAll(String bairro) {
-
-    if(bairro == null){
-      return matriculaRepository.findAll();
-    }else{
-      return matriculaRepository.findAlunosMatriculadosBairro(bairro);
+    @Autowired
+    public MatriculaServiceImpl(MatriculaRepository matriculaRepository, AlunoRepository alunoRepository) {
+        this.matriculaRepository = matriculaRepository;
+        this.alunoRepository = alunoRepository;
     }
 
-  }
+    @Override
+    public Matricula create(MatriculaForm form) {
+        Aluno aluno = alunoRepository.findById(form.getAlunoId())
+                .orElseThrow(() -> new IllegalArgumentException("Aluno não encontrado"));
 
-  @Override
-  public void delete(Long id) {}
+        Matricula matricula = new Matricula(aluno);
 
+        return matriculaRepository.save(matricula);
+    }
 
+    @Override
+    public Matricula get(Long id) {
+        return matriculaRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Matrícula não encontrada"));
+    }
 
+    @Override
+    public List<Matricula> getAll(String bairro) {
+        if (bairro == null) {
+            return matriculaRepository.findAll();
+        } else {
+            return matriculaRepository.findAlunosMatriculadosBairro(bairro);
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        Optional<Matricula> optionalMatricula = matriculaRepository.findById(id);
+        if (optionalMatricula.isPresent()) {
+            matriculaRepository.delete(optionalMatricula.get());
+        } else {
+            throw new IllegalArgumentException("Matrícula não encontrada");
+        }
+    }
 }

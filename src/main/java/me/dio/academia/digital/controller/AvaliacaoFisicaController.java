faeedd901c718567aa749dd1abe5d25a79a90954
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-04-28 15:26:39 -0300

    Update AvaliacaoFisicaController.java

--- src/main/java/me/dio/academia/digital/controller/AvaliacaoFisicaController.java
+++ src/main/java/me/dio/academia/digital/controller/AvaliacaoFisicaController.java
@@ -1,28 +1,70 @@
 package me.dio.academia.digital.controller;
 
-import me.dio.academia.digital.entity.AvaliacaoFisica;
-import me.dio.academia.digital.entity.form.AvaliacaoFisicaForm;
-import me.dio.academia.digital.service.impl.AvaliacaoFisicaServiceImpl;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaForm;
+import me.dio.academia.digital.service.AvaliacaoFisicaService;
 
 @RestController
 @RequestMapping("/avaliacoes")
+@Validated
 public class AvaliacaoFisicaController {
 
+  private final AvaliacaoFisicaService avaliacaoFisicaService;
+
   @Autowired
-  private AvaliacaoFisicaServiceImpl service;
+  public AvaliacaoFisicaController(AvaliacaoFisicaService avaliacaoFisicaService) {
+      this.avaliacaoFisicaService = avaliacaoFisicaService;
+  }
 
   @PostMapping
-  public AvaliacaoFisica create(@RequestBody AvaliacaoFisicaForm form) {
-    return service.create(form);
+  @ResponseStatus(HttpStatus.CREATED)
+  public AvaliacaoFisica create(@Valid @RequestBody AvaliacaoFisicaForm form) {
+    return avaliacaoFisicaService.create(form);
   }
 
   @GetMapping
-  public List<AvaliacaoFisica> getAll(){
-    return service.getAll();
+  public ResponseEntity<List<AvaliacaoFisica>> getAll(){
+    List<AvaliacaoFisica> avaliacoes = avaliacaoFisicaService.getAll();
+    if(avaliacoes.isEmpty()) {
+        return ResponseEntity.noContent().build();
+    }
+    return ResponseEntity.ok(avaliacoes);
   }
 
+  @GetMapping("/{id}")
+  public ResponseEntity<AvaliacaoFisica> getById(@PathVariable Long id) {
+    AvaliacaoFisica avaliacao = avaliacaoFisicaService.getById(id);
+    if(avaliacao == null) {
+        return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(avaliacao);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<AvaliacaoFisica> update(@PathVariable Long id, @Valid @RequestBody AvaliacaoFisicaForm form) {
+    AvaliacaoFisica avaliacao = avaliacaoFisicaService.update(id, form);
+    if(avaliacao == null) {
+        return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(avaliacao);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    boolean deleted = avaliacaoFisicaService.delete(id);
+    if(!deleted) {
+        return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.noContent().build();
+  }
 }

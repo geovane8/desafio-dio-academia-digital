28a27e4758ffcfc815a1f2b0418348edf696b6cb
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-04-28 15:53:09 -0300

    Update JavaTimeUtils.java

--- src/main/java/me/dio/academia/digital/infra/utils/JavaTimeUtils.java
+++ src/main/java/me/dio/academia/digital/infra/utils/JavaTimeUtils.java
@@ -3,16 +3,22 @@ package me.dio.academia.digital.infra.utils;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
-/**
- * @author Venilton Falvo Jr
- */
-public class JavaTimeUtils {
-	
-	private JavaTimeUtils() {}
-	
-	private  static final Locale LOCALE_BR = new Locale("pt", "BR");
-	
-	public static final DateTimeFormatter LOCAL_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss", LOCALE_BR);
-	public static final DateTimeFormatter LOCAL_DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy", LOCALE_BR);
+public final class JavaTimeUtils {
+    private JavaTimeUtils() {
+        throw new AssertionError("This class should not be instantiated");
+    }
 
+    private static final Locale LOCALE_BR = Locale.forLanguageTag("pt-BR");
+    private static final DateTimeFormatter LOCAL_DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss", LOCALE_BR);
+    private static final DateTimeFormatter LOCAL_DATE_FORMATTER =
+            DateTimeFormatter.ofPattern("dd/MM/yyyy", LOCALE_BR);
+
+    public static DateTimeFormatter localDateTimeFormatter() {
+        return LOCAL_DATE_TIME_FORMATTER;
+    }
+
+    public static DateTimeFormatter localDateFormatter() {
+        return LOCAL_DATE_FORMATTER;
+    }
 }

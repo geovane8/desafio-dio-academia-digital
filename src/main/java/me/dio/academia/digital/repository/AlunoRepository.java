ddbcaa3879e58327fc38f63b74d83f7359cf5ee8
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-04-28 15:55:25 -0300

    Update AlunoRepository.java
    Foi adicionado um método de consulta personalizado para buscar todos os alunos cuja data de nascimento está entre duas datas, utilizando a anotação @Query do Spring Data JPA. Esse método aceita dois parâmetros, um início e um fim, que são especificados através das anotações @Param.
    
    Foi adicionado o modificador public ao método personalizado findAllByDataDeNascimentoBetween(), que é necessário para que ele possa ser acessado por outras classes fora do pacote.

--- src/main/java/me/dio/academia/digital/repository/AlunoRepository.java
+++ src/main/java/me/dio/academia/digital/repository/AlunoRepository.java
@@ -1,20 +1,20 @@
 package me.dio.academia.digital.repository;
 
-import me.dio.academia.digital.entity.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.time.LocalDate;
 import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import me.dio.academia.digital.entity.Aluno;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 
-  /**
-   *
-   * @param dataDeNascimento: data de nascimento dos alunos
-   * @return lista com todos os alunos com a data de nascimento passada como parâmetro da função
-   */
   List<Aluno> findByDataDeNascimento(LocalDate dataDeNascimento);
 
+  @Query("SELECT a FROM Aluno a WHERE a.dataDeNascimento BETWEEN :start AND :end")
+  List<Aluno> findAllByDataDeNascimentoBetween(@Param("start") LocalDate start, @Param("end") LocalDate end);
 }

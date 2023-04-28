9afdc574f942436619f9e79160c8ca05f15fbb48
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-04-28 16:04:54 -0300

    Update AlunoServiceImpl.java

--- src/main/java/me/dio/academia/digital/service/impl/AlunoServiceImpl.java
+++ src/main/java/me/dio/academia/digital/service/impl/AlunoServiceImpl.java
@@ -1,5 +1,12 @@
 package me.dio.academia.digital.service.impl;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import me.dio.academia.digital.entity.Aluno;
 import me.dio.academia.digital.entity.AvaliacaoFisica;
 import me.dio.academia.digital.entity.form.AlunoForm;
@@ -7,62 +14,56 @@ import me.dio.academia.digital.entity.form.AlunoUpdateForm;
 import me.dio.academia.digital.infra.utils.JavaTimeUtils;
 import me.dio.academia.digital.repository.AlunoRepository;
 import me.dio.academia.digital.service.IAlunoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Service
 public class AlunoServiceImpl implements IAlunoService {
 
-  @Autowired
-  private AlunoRepository repository;
+    private final AlunoRepository repository;
 
-  @Override
-  public Aluno create(AlunoForm form) {
-    Aluno aluno = new Aluno();
-    aluno.setNome(form.getNome());
-    aluno.setCpf(form.getCpf());
-    aluno.setBairro(form.getBairro());
-    aluno.setDataDeNascimento(form.getDataDeNascimento());
-
-    return repository.save(aluno);
-  }
-
-  @Override
-  public Aluno get(Long id) {
-    return null;
-  }
-
-  @Override
-  public List<Aluno> getAll(String dataDeNascimento) {
-
-    if(dataDeNascimento == null) {
-      return repository.findAll();
-    } else {
-      LocalDate localDate = LocalDate.parse(dataDeNascimento, JavaTimeUtils.LOCAL_DATE_FORMATTER);
-      return repository.findByDataDeNascimento(localDate);
+    @Autowired
+    public AlunoServiceImpl(AlunoRepository repository) {
+        this.repository = repository;
     }
 
-  }
+    @Override
+    public Aluno create(AlunoForm form) {
+        Aluno aluno = new Aluno(form.getNome(), form.getCpf(), form.getBairro(), form.getDataDeNascimento());
+        return repository.save(aluno);
+    }
 
-  @Override
-  public Aluno update(Long id, AlunoUpdateForm formUpdate) {
-    return null;
-  }
+    @Override
+    public Optional<Aluno> get(Long id) {
+        return repository.findById(id);
+    }
 
-  @Override
-  public void delete(Long id) {
-  }
+    @Override
+    public List<Aluno> getAll(LocalDate dataDeNascimento) {
+        return dataDeNascimento != null ? repository.findByDataDeNascimento(dataDeNascimento)
+                : repository.findAll();
+    }
 
-  @Override
-  public List<AvaliacaoFisica> getAllAvaliacaoFisicaId(Long id) {
+    @Override
+    public Optional<Aluno> update(Long id, AlunoUpdateForm formUpdate) {
+        Optional<Aluno> optionalAluno = repository.findById(id);
+        optionalAluno.ifPresent(aluno -> {
+            aluno.setNome(formUpdate.getNome());
+            aluno.setCpf(formUpdate.getCpf());
+            aluno.setBairro(formUpdate.getBairro());
+            aluno.setDataDeNascimento(formUpdate.getDataDeNascimento());
+            repository.save(aluno);
+        });
+        return optionalAluno;
+    }
 
-    Aluno aluno = repository.findById(id).get();
+    @Override
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 
-    return aluno.getAvaliacoes();
-
-  }
+    @Override
+    public List<AvaliacaoFisica> getAllAvaliacaoFisica(Long id) {
+        Optional<Aluno> optionalAluno = repository.findById(id);
+        return optionalAluno.map(Aluno::getAvaliacoes).orElse(List.of());
+    }
 
 }
